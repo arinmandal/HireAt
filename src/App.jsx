ce4ed52b88911864a13e8133f09e36314bfcdb8f
Arin Mandal <arinmandal40@gmail.com>
2026-01-29 23:58:21 +0530

    feat: setup routing architecture and initial page structures

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,51 @@
 import React from "react";
+import Rootlayout from "./layout/Rootlayout";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Onboarding from "./pages/Onboarding";
+import SavedJobs from "./pages/SavedJobs";
+import PostJobs from "./pages/PostJobs";
+import JobListing from "./pages/JobListing";
+import Jobs from "./pages/Jobs";
+import MyJobs from "./pages/MyJobs";
 const App = () => {
-	return (
-		<div>
-			<h1>HireAT</h1>
-		</div>
-	);
+	const router = createBrowserRouter([
+		{
+			element: <Rootlayout />,
+			children: [
+				{
+					path: "/",
+					element: <HomePage />,
+				},
+				{
+					path: "/onboarding",
+					element: <Onboarding />,
+				},
+				{
+					path: "/joblisting",
+					element: <JobListing />,
+				},
+				{
+					path: "/job/:id",
+					element: <Jobs />,
+				},
+				{
+					path: "/saved-jobs",
+					element: <SavedJobs />,
+				},
+				{
+					path: "/post-jobs",
+					element: <PostJobs />,
+				},
+				{
+					path: "/my-jobs",
+					element: <MyJobs />,
+				},
+			],
+		},
+	]);
+
+	return <RouterProvider router={router} />;
 };
 
 export default App;
